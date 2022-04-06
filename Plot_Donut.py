d073427273147eb4b5fec78b528cb559ab0eadb3
Narmin <64002842+narminGhaffari@users.noreply.github.com>
2022-04-06 09:58:13 +0200

    Bar Graph

--- Plot_Donut.py
+++ Plot_Donut.py
@@ -16,24 +16,22 @@ import numpy as np
 def PlotDonut (subsetValues, savePath, color = ['#a50026', '#fdae61'], figTitle = 'Donut Figure'):
     
     """
-        Plots donut figure. The inpu to the function can be single list or the list of lists to plot and save the figures automatically. 
+        Plots donut figure. The input to the function can be single list or the list of lists to plot and save the figures automatically. 
         Arguments:
-            subsetValues: is a list containing the values for the donut figure.
-            
-            example 1: subsetValues = [10, 20]            
-            savePath: a path containing the image name to save the figure.
-            color: list of colors to fill the parts of donut figure.
-            figTitle: the title of the figure.
+            subsetValues: is a list containing the values for the donut figure.            
+            example: subsetValues = [10, 20]            
+            savePath: a path containing the image name to save the figure.            
+            color: list of colors to fill the parts of donut figure.            
+            figTitle: the title of the figure.            
         Returns:
             A donut figure saved in the specified directory.
-    """        
+    """     
+    
     assert len(subsetValues) == len(color)   
      
     fig, ax = plt.subplots()
     mpl.rcParams['font.size'] = 10
-    #ax.axis('equal')
-    mypie,texts = ax.pie(subsetValues, radius=1,pctdistance = 1.4, colors = color)
-    
+    mypie,texts = ax.pie(subsetValues, radius=1,pctdistance = 1.4, colors = color)    
     plt.setp(mypie, width=0.5, edgecolor='white')
     ax.set_title(figTitle)
     
@@ -48,10 +46,8 @@ def PlotDonut (subsetValues, savePath, color = ['#a50026', '#fdae61'], figTitle 
         connectionstyle = "angle,angleA=0,angleB={}".format(ang)
         kw["arrowprops"].update({"connectionstyle": connectionstyle})
         ax.annotate(str(subsetValues[i]) + '(' + str(np.round(subsetValues[i] / np.sum(subsetValues),2)*100) + '%)',
-                    xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),horizontalalignment=horizontalalignment, **kw)
-    
+                    xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y), horizontalalignment=horizontalalignment, **kw)
     ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
-
     plt.savefig(savePath, bbox_inches='tight', dpi=500, pad_inches = 0.5)
     plt.close()
 
